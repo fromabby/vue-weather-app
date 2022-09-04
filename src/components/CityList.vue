08f50612c7cb5cb030419bb44e4284e0c9888930
fromabby <abbonuan@gmail.com>
2022-09-04 18:12:08 +0800

    removed cors url

--- src/components/CityList.vue
+++ src/components/CityList.vue
@@ -26,7 +26,7 @@ const getCities = async () => {
 
         savedCities.value.forEach(({ coords: {lat,lon}}) => {
             requests.push(
-                axios.get(`https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+                axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
             );
         });
 
